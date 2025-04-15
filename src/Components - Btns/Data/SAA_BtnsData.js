f74026436b6530f2fbdbf47508c4e9dfd921edd4
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-15 19:40:11 -0400

    A new data array added

---
+++ src/Components - Btns/Data/SAA_BtnsData.js
@@ -0,0 +1,40 @@
+export const SAA_BtnDataUnder = [
+    {
+        name: 'Asuntos urgente',
+        function: () => {},
+        Style: 'border-1 p-1 rounded-md bg-blue-500 text-white',
+    },
+    {
+        name: 'Otras solicitudes',
+        function: () => {},
+        Style: 'border-2 border-gray-300 p-1 rounded-md text-gray-500',
+    },
+    {
+        name: 'Todos',
+        function: () => {},
+        Style: 'border-2 border-gray-300 p-1 rounded-md text-gray-500',
+    },
+]
+
+export const SAA_BtnDataBelow = [
+    {
+        name: 'Sorteo aleatorio',
+        function: () => {},
+        Style: '',
+    },
+    {
+        name: 'Rovocar asignacion',
+        function: () => {},
+        Style: '',
+    },
+    {
+        name: 'Limpiar filtro',
+        function: () => {},
+        Style: '',
+    },
+    {
+        name: 'Mostrar mas registros',
+        function: () => {},
+        Style: '',
+    }
+]
