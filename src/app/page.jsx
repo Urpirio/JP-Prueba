3adf41393d7a6690047168fab9bcb9ec386adc02
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-15 19:32:28 -0400

    Working in Home and i added a new feature

---
+++ src/app/page.jsx
@@ -0,0 +1,61 @@
+import BtnConfiguration from "@/Components - Btns/BtnConfiguration";
+import { BtnNavBlock } from "@/Components - Btns/NavBlock/BtnNavBlock";
+import { BtnBelow, BtnUnder } from "@/Components - Btns/SAA_Btns/Btns/Btns";
+import Search_Filtro from "@/Components - Btns/SAA_Filtro/Search_Filtro";
+import Select_Asunto from "@/Components - Btns/SAA_Selects/Select_Asunto";
+import Select_Criterio from "@/Components - Btns/SAA_Selects/Select_Criterio";
+import Select_Juez from "@/Components - Btns/SAA_Selects/Select_Juez";
+import Select_Procedimiento from "@/Components - Btns/SAA_Selects/Select_Procedimiento";
+import Select_Sala from "@/Components - Btns/SAA_Selects/Select_Sala";
+
+
+export default function Home() {
+  return (
+   <main className="border-1 p-5">
+
+    <section className="border-1">
+
+      <div className="flex justify-end">
+        <BtnConfiguration/>
+      </div>
+
+      <div>
+        {/* Navegacion */}
+        <nav className="flex border-1">
+          <BtnNavBlock/>
+        </nav>
+        {/* Formulario */}
+        <div className="flex flex-col py-5">
+
+          <div className="px-5">
+            <h2>SORTEO A ABOGADO AYUDANTE</h2>
+          </div>
+
+          <div className="flex gap-2 py-5 px-5">
+            <BtnUnder/>
+          </div>
+
+          <div className="flex justify-around px-1 gap-10 w-[100%] mb-5">
+            <Select_Sala/>
+            <Select_Juez/>
+          </div>
+
+          <div className="flex justify-around px-1 w-[100%]">
+              <Select_Procedimiento/>
+              <Select_Asunto/>
+              <Select_Criterio/>
+              <Search_Filtro/>
+          </div>
+
+          <div>
+            
+          </div>
+
+        </div>
+      </div>
+
+    </section>
+    
+   </main>
+  )
+};
