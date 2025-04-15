d30805c2e6907ab2aa37d002fda0a2a55c99fb6f
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-15 19:51:24 -0400

    Section than content a perfil in aside

---
+++ src/SubComponents/AsidePerfil.jsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import PhotoPerfil from '@/../public/Perfil/PerfilPhoto.png';
+
+export default function AsidePerfil() {
+  return (
+    <section className="flex flex-col items-center">
+        <div className="border-1 rounded-[100%] ">
+            <Image className="h-30 w-30 rounded-[100%] object-contain" src={PhotoPerfil} alt="Img Perfil"/>
+        </div>
+        <div className="
+        flex
+        flex-col
+        text-center
+        [&div>h2]:text-2xl
+        [&div>h2]:text-blue-600
+        [&div>span]:text-xs
+
+        ">
+            <h2>Sandy Garcia</h2>
+            <span>Administrador</span>
+        </div>
+    </section>
+  )
+}
