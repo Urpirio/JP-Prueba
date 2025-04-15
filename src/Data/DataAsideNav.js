70d162bdcfff52ab009b63352447fd3f4f41cf03
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-15 19:49:57 -0400

    I added a new Data array  for Navigation in Aside

---
+++ src/Data/DataAsideNav.js
@@ -0,0 +1,56 @@
+import { AiOutlineHome } from "react-icons/ai";
+import { FaRegFileAlt } from "react-icons/fa";
+import { FaRegFile } from "react-icons/fa";
+
+export const DataAsideNav = [
+    {
+        key: 1,
+        Icon: <AiOutlineHome/>,
+        Title: 'Inicio',
+    },
+    {
+        key: 2,
+        Icon: <FaRegFileAlt/>,
+        Title: 'Deposito',
+    },
+    {
+        key: 3,
+        Icon: <FaRegFile/>,
+        Title: 'Registrar',
+    },
+    {
+        key: 4,
+        Icon: <FaRegFile/>,
+        Title: 'Devueltos',
+    },
+    {
+        key: 5,
+        Icon: <FaRegFile/>,
+        Title: 'Historicos',
+    },
+    {
+        key: 6,
+        Icon: <FaRegFile/>,
+        Title: 'Agente de servicio',
+    },
+    {
+        key: 7,
+        Icon: <FaRegFile/>,
+        Title: 'Expedientes',
+    },
+    {
+        key: 8,
+        Icon: <FaRegFile/>,
+        Title: 'Solicitudes',
+    },
+    {
+        key: 9,
+        Icon: <FaRegFile/>,
+        Title: 'Solicitud Digitalizacion',
+    },
+    {
+        key: 10,
+        Icon: <FaRegFile/>,
+        Title: 'Digitalizacion Buzon',
+    }
+];
