9a5c525ef395330384ff8cbcceb8ffc7b10805a7
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-15 19:37:42 -0400

    was added a new btn, than have as name

---
+++ src/Components - Btns/Data/NavBlock_BtnData.js
@@ -0,0 +1,26 @@
+export const NavBlock_BtnData = [
+    {
+        key: 1,
+        name: 'Sorteo a Salas',
+        style: 'p-1 rounded-t-xl',
+        function: ()=>{},
+    },
+    {
+        key: 2,
+        name: 'Sorteo a Jueces',
+        style: 'p-1 rounded-t-xl',
+        function: ()=>{},
+    },
+    {
+        key: 3,
+        name: 'Sorteo a Abogado ayudante',
+        style: 'border-t-1 border-l-1 border-r-1 p-1 rounded-t-xl',
+        function: ()=>{},
+    },
+    {
+        key: 4,
+        name: 'Sorteo a Admisibilidad',
+        style: 'p-1 rounded-t-xl',
+        function: ()=>{},
+    }
+];
