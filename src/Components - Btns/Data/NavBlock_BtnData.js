0e20efef4e9b9260a178584766f841036cf17db2
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-16 08:32:08 -0400

    I have improved the style button in the navigation

--- src/Components - Btns/Data/NavBlock_BtnData.js
+++ src/Components - Btns/Data/NavBlock_BtnData.js
@@ -2,25 +2,25 @@ export const NavBlock_BtnData = [
     {
         key: 1,
         name: 'Sorteo a Salas',
-        style: 'p-1 rounded-t-xl',
+        style: 'p-2 rounded-t-xs text-[14px]',
         function: ()=>{},
     },
     {
         key: 2,
         name: 'Sorteo a Jueces',
-        style: 'p-1 rounded-t-xl',
+        style: 'p-2 rounded-t-xs text-[14px]',
         function: ()=>{},
     },
     {
         key: 3,
         name: 'Sorteo a Abogado ayudante',
-        style: 'border-t-1 border-l-1 border-r-1 p-1 rounded-t-xl',
+        style: 'border-t-1 text-[14px] border-l-1 border-r-1 border-blue-500 p-2 z-10 bg-white  rounded-t-xs',
         function: ()=>{},
     },
     {
         key: 4,
         name: 'Sorteo a Admisibilidad',
-        style: 'p-1 rounded-t-xl',
+        style: 'p-2 rounded-t-xs text-[14px]',
         function: ()=>{},
     }
 ];
