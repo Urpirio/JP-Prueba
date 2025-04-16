46d8c85f10a2554ff794b084461b3599d62c435a
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-16 09:55:09 -0400

    I added a new buttons

---
+++ src/Components - Btns/SAA_Btns/Btns/BtnsNav.jsx
@@ -0,0 +1,11 @@
+
+export const BtnNext = () => {
+    return(
+        <button className="border-b-1 border-r-1 border-t-1 border-gray-400 text-blue-500 px-2 py-1 rounded-r-xl hover:opacity-80 hover:bg-gray-200 cursor-pointer">Siguiente</button>
+    )
+}
+export const BtnBack = () => {
+    return(
+        <button className="border-b-1 border-l-1 border-t-1 border-gray-400 px-2 py-1 rounded-l-xl hover:opacity-80 hover:bg-gray-200 cursor-pointer">Anterior</button>
+    )
+}
