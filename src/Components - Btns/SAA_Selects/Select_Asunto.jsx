ad0c4186e480330ba6b2fabc7318fd511ef26396
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-16 08:37:24 -0400

    I have improved the style of the button and the icon inside the button, I will make a new functionality for each one.

--- src/Components - Btns/SAA_Selects/Select_Asunto.jsx
+++ src/Components - Btns/SAA_Selects/Select_Asunto.jsx
@@ -2,13 +2,13 @@ import { IoMdArrowDropright } from "react-icons/io";
 
 export default function Select_Asunto() {
   return (
-    <div className="flex flex-col border-1">
+    <div className="flex flex-col">
         <div>
-            <span>Asunto</span>
+            <span className="text-gray-600 text-[14px]">Asunto<i className="text-red-600 font-bold">*</i></span>
         </div>
-        <div className="flex justify-between items-center border-1 p-1.5 w-50">
-            <span className="text-[14px]">Seleccionar asunto</span>
-            <IoMdArrowDropright/>
+        <div className="flex justify-between items-center border-1 border-gray-300  rounded-xs p-1.5 w-50">
+            <span className="text-[14px] text-gray-500">Seleccionar asunto</span>
+            <IoMdArrowDropright className="text-gray-500"/>
         </div>
     </div>
   )
