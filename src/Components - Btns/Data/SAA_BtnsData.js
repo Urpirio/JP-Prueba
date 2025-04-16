009b342cb979e935a573a954b7435a22cc4499fe
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-16 08:34:55 -0400

    I have improve the style buttons and later i will add a news functionality

--- src/Components - Btns/Data/SAA_BtnsData.js
+++ src/Components - Btns/Data/SAA_BtnsData.js
@@ -2,17 +2,17 @@ export const SAA_BtnDataUnder = [
     {
         name: 'Asuntos urgente',
         function: () => {},
-        Style: 'border-1 p-1 rounded-md bg-blue-500 text-white',
+        Style: 'border-1 p-1 rounded-xs bg-blue-500 w-35 text-white',
     },
     {
         name: 'Otras solicitudes',
         function: () => {},
-        Style: 'border-2 border-gray-300 p-1 rounded-md text-gray-500',
+        Style: 'border-1 border-gray-300 w-35 rounded-xs text-gray-500',
     },
     {
         name: 'Todos',
         function: () => {},
-        Style: 'border-2 border-gray-300 p-1 rounded-md text-gray-500',
+        Style: 'border-1 border-gray-300 w-20 rounded-xs text-[14px] text-gray-500',
     },
 ]
 
@@ -20,21 +20,21 @@ export const SAA_BtnDataBelow = [
     {
         name: 'Sorteo aleatorio',
         function: () => {},
-        Style: '',
+        Style: 'border-1 py-1.5 px-3 rounded-xs bg-[#69D56D] text-white text-[14px] hover:opacity-75 hover:cursor-pointer',
     },
     {
         name: 'Rovocar asignacion',
         function: () => {},
-        Style: '',
+        Style: 'border-1 py-1.5 px-3 rounded-xs bg-[#FBB55E] text-white text-[14px] hover:opacity-75 hover:cursor-pointer',
     },
     {
         name: 'Limpiar filtro',
         function: () => {},
-        Style: '',
+        Style: 'border-1 py-1.5 px-3 rounded-xs bg-[#FF737B] text-white text-[14px] hover:opacity-75 hover:cursor-pointer',
     },
     {
         name: 'Mostrar mas registros',
         function: () => {},
-        Style: '',
+        Style: 'border-1 py-1.5 px-3 rounded-xs bg-[#3371F1] text-white text-[14px] hover:opacity-75 hover:cursor-pointer',
     }
 ]
