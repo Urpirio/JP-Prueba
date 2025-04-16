075ff920180c2f8cc94c0b9472fe00f32750f8d2
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-16 09:55:30 -0400

    I completed footer component

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,6 +1,13 @@
 
 export default function Footer() {
   return (
-    <div></div>
+    <footer>
+      <section className="flex flex-col items-center text-center py-1">
+        <div className="border-b-2 border-blue-700">
+          <span>Puede comunicarse con la mesa de servicios utilizando el correo: <a className="text-blue-500" href="mailto:mesadeservicios@poderjudicial.god.do">mesadeservicios@poderjudicial.god.do</a></span> 
+          </div>
+          <span>CopyRight 2024. Todos los derechos reservados.</span>
+      </section>
+    </footer>
   )
 }
