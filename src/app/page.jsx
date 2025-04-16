09c8a27d64a25c46b654f828d393575bfaab3dc7
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-16 08:29:15 -0400

    I have improved style and optimized

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,31 +7,36 @@ import Select_Criterio from "@/Components - Btns/SAA_Selects/Select_Criterio";
 import Select_Juez from "@/Components - Btns/SAA_Selects/Select_Juez";
 import Select_Procedimiento from "@/Components - Btns/SAA_Selects/Select_Procedimiento";
 import Select_Sala from "@/Components - Btns/SAA_Selects/Select_Sala";
+import Table from "@/Components - Btns/SAA_Table/Table";
 
 
 export default function Home() {
   return (
-   <main className="border-1 p-5">
+   <main className="  p-5">
 
-    <section className="border-1">
+    <section className="">
 
       <div className="flex justify-end">
         <BtnConfiguration/>
       </div>
 
-      <div>
+      <div >
         {/* Navegacion */}
-        <nav className="flex border-1">
-          <BtnNavBlock/>
+        <nav className="flex justify-center items-baseline-last relative">
+          <div className="flex w-[100%]">
+            <BtnNavBlock/>
+          </div>
+          <div className="h-[2px] rounded-2xl w-[100%] bg-blue-600 absolute"></div>
         </nav>
         {/* Formulario */}
-        <div className="flex flex-col py-5">
-
-          <div className="px-5">
+        <div className="px-5 py-2 text-gray-600 border-b-2 border-l-1 border-r-1 border-gray-300">
             <h2>SORTEO A ABOGADO AYUDANTE</h2>
           </div>
+        <div className="flex flex-col border-b-1 border-l-1 border-r-1 py-2 border-gray-300 rounded-b-xs rounded-xs shadow-xl">
 
-          <div className="flex gap-2 py-5 px-5">
+          
+
+          <div className="flex gap-2 py-2 px-5">
             <BtnOver/>
           </div>
 
@@ -47,8 +52,12 @@ export default function Home() {
               <Search_Filtro/>
           </div>
 
-          <div>
+          <div className="flex justify-end p-5 gap-2">
+            <BtnBelow/>
+          </div>
 
+          <div className="flex justify-center p-5">
+            <Table/>
           </div>
 
         </div>
