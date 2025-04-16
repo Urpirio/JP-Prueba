b35747dbad4b62a1abe343dcfff2de4f7ae67664
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-16 08:41:50 -0400

    I have improved the button style and i planning to add  a news functionality

--- src/Components - Btns/SAA_Selects/Select_Sala.jsx
+++ src/Components - Btns/SAA_Selects/Select_Sala.jsx
@@ -2,13 +2,13 @@ import { IoMdArrowDropright } from "react-icons/io";
 
 export default function Select_Sala() {
   return (
-    <div className="flex flex-col justify-center w-[45%] border-1">
+    <div className="flex flex-col justify-center w-[45%]">
         <div>
-            <span>Sala</span>
+            <span className="text-gray-600 text-[14px]">Sala<i className="text-red-600 font-bold">*</i></span>
         </div>
-        <div className="flex justify-between items-center border-1 p-1 ">
-            <span>Seleccionar sala</span>
-            <IoMdArrowDropright/>
+        <div className="flex justify-between items-center border-1 border-gray-300  rounded-xs p-1 ">
+            <span className="text-gray-500">Seleccionar sala</span>
+            <IoMdArrowDropright className="text-gray-500"/>
         </div>
     </div>
   )
