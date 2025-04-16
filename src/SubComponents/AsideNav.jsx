d90bf53ed3325a2eb63fe47a50e9376e37eca88f
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-16 09:00:55 -0400

    I have improved the button navigation in Aside nav

--- src/SubComponents/AsideNav.jsx
+++ src/SubComponents/AsideNav.jsx
@@ -5,7 +5,7 @@ export default function AsideNav() {
 
   const BtnNavegacion = () => {
     const listBtnNav = DataAsideNav.map( DAN => {
-      return(<li key={DAN.key} className="flex items-center gap-1 border-1 text-[18px] p-1">{DAN.Icon}<span>{DAN.Title}</span></li>)
+      return(<li key={DAN.key} className="flex items-center gap-1 text-[15px] p-1.5 hover:bg-blue-200 hover:text-white cursor-pointer rounded-md w-[100%]">{DAN.Icon}<span>{DAN.Title}</span></li>)
     })
     return(listBtnNav)
   };
@@ -13,8 +13,8 @@ export default function AsideNav() {
 
 
   return (
-    <nav>
-        <ul className="flex flex-col gap-1">
+    <nav className=" h-[100%] w-[100%] flex justify-center">
+        <ul className="flex flex-col gap-1 w-[100%]">
           <BtnNavegacion/>
         </ul>
     </nav>
