fec6a44fc22635f4a7fc4ea4ca1c8038dd288251
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-16 08:35:26 -0400

    I have improved the style button and input

--- src/Components - Btns/SAA_Filtro/Search_Filtro.jsx
+++ src/Components - Btns/SAA_Filtro/Search_Filtro.jsx
@@ -2,13 +2,13 @@ import { IoSearchOutline } from "react-icons/io5";
 
 export default function Search_Filtro() {
   return (
-    <div className="border-1">
+    <div className="flex flex-col">
       <div>
-        <span>Filtro generico</span>
+        <span className="text-gray-600 text-[14px]">Filtro generico<i className="text-red-600 font-bold">*</i></span>
       </div>
-      <div className="flex items-center border-1">
+      <div className="flex items-center border-1 border-gray-300 rounded-xs">
         <input type="text" className="p-1 outline-none" placeholder="Ingrese el criterio al filtrar" />
-        <button className="p-1">
+        <button className="p-2 border-1 border-gray-400  rounded-r-xs bg-gray-400 text-white">
             <IoSearchOutline/>
         </button>
       </div>
