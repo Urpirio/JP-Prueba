87ecafa5601ce570480f835ea342dd4dea227d75
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-04-16 08:41:03 -0400

    I have improved the button style and i'm planning to add a new functionality

--- src/Components - Btns/SAA_Selects/Select_Procedimiento.jsx
+++ src/Components - Btns/SAA_Selects/Select_Procedimiento.jsx
@@ -2,13 +2,13 @@ import { IoMdArrowDropright } from "react-icons/io";
 
 export default function Select_Procedimiento() {
   return (
-    <div className="flex flex-col border-1">
+    <div className="flex flex-col">
         <div>
-            <span>Procedimiento</span>
+            <span className="text-gray-600 text-[14px]">Procedimiento<i className="text-red-600 font-bold">*</i></span>
         </div>
-        <div className="flex justify-between items-center border-1 p-1.5 ">
-            <span className="text-[14px]">Seleccionar tipo de Procedimiento</span>
-            <IoMdArrowDropright/>
+        <div className="flex justify-between items-center border-1 border-gray-300  rounded-xs p-1.5 ">
+            <span className="text-[14px] text-gray-500">Seleccionar tipo de Procedimiento</span>
+            <IoMdArrowDropright className="text-gray-500"/>
         </div>
     </div>
   )
